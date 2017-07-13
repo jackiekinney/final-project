f5f631a45e21d2cadfac6806c8654097e6eb2d1a
jackiekinney <JacquelineKinney@Jacqueline-Kinneys-MacBook-Pro.local>
2017-07-12 19:40:51 -0700

    add all files

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// Slick carousel behavior
+
+$(document).ready(function(){
+      $('.responsive').slick({
+      arrows: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+  });
+});
+
+/* DROP DOWN MENUS --- When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+/*
+function myFunction() {
+    document.getElementById("leafyGreens").classList.toggle("show");
+}
+*/
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+$('#fruit').on('click', function() {
+  $('#fruitDpdn').toggleClass('show');
+});
+
+$('#fats').on('click', function() {
+  $('#fatsDpdn').toggleClass('show');
+});
